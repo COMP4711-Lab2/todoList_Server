836e7325851aa488f89fd690c0b93dd3d5b05920
Stieltjes <SRayleigh@gmail.com>
2017-11-30 10:22:06 -0800

    Crud implementation

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -133,3 +133,5 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array('app', 'tasks');
+
+$autoload['packages'] = array(APPPATH.'third_party/restful/');
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -30,14 +30,4 @@ class Application extends CI_Controller
 	/**
 	 * Render this page
 	 */
-	function render($template = 'template')
-	{
-        $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
-		
-		// use layout content if provided
-		if (!isset($this->data['content']))
-			$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-		$this->parser->parse($template, $this->data);
-	}
-
 }
